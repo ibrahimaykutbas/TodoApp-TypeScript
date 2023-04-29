82652a4edf571951f9f6ce78bfbd48fa8a576ac4
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2023-04-29 13:41:06 +0300

    Add Todo

---
+++ src/components/Todo.tsx
@@ -0,0 +1,40 @@
+import { Pressable, Text, StyleSheet } from 'react-native';
+import React, { FC } from 'react';
+
+interface ITodo {
+  todo: {
+    id: number;
+    value: string;
+  };
+  deleteTodo: (id: number) => void;
+}
+
+const Todo: FC<ITodo> = ({ todo, deleteTodo }) => {
+  return (
+    <Pressable
+      style={styles.todo}
+      key={todo.id}
+      onLongPress={() => deleteTodo(todo.id)}>
+      <Text style={styles.todoTitle}>{todo.value}</Text>
+    </Pressable>
+  );
+};
+
+export default Todo;
+
+const styles = StyleSheet.create({
+  todo: {
+    height: 50,
+    backgroundColor: '#FFE6C7',
+    marginTop: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    justifyContent: 'center',
+    paddingLeft: 10
+  },
+  todoTitle: {
+    color: '#454545',
+    fontWeight: 'bold',
+    fontSize: 15
+  }
+});
